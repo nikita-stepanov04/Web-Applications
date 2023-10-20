b7b8fbc245e05be1ac5d83bbf7944c0e8c67b1d3
nikita-stepanov04 <nikita.stepanov556789@gmail.com>
2023-10-20 12:55:50 +0300

    Add additional validation for text inputs

--- Лабораторна робота6/js/form.js
+++ Лабораторна робота6/js/form.js
@@ -1,7 +1,20 @@
+//mask for phones
 const tels = document.querySelectorAll('#phone');
 const inputMask = new Inputmask('+380 (99) 999-99-99');
 inputMask.mask(tels);
 
+//restrict date in form
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+const year = yesterday.getFullYear();
+const month = String(yesterday.getMonth() + 1).padStart(2, '0');
+const day = String(yesterday.getDate()).padStart(2, '0');
+const formattedDate = year + '-' + month + '-' + day;
+console.log(formattedDate)
+document.querySelector('#birthday').setAttribute('max', formattedDate);
+
+//validation form
 const form = document.querySelector('.needs-validation');
 form.addEventListener('submit', event => {
     if (!form.checkValidity()) {
